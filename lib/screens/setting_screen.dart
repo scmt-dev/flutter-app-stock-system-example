d0bba9a69fe25ac25c76ca86482bb4d772181e14
Mark AI <chanthavong_12@hotmail.com>
2024-03-26 10:03:25 +0700

    improve(ui): add sign in ui

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool isDarkTheme = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Setting'),),
+      appBar: AppBar(
+        title: Text('Setting'),
+      ),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: const Text('Dark Mode'),
+            value: isDarkTheme,
+            onChanged: (val) {
+              setState(() {
+                isDarkTheme = val;
+              });
+              print(isDarkTheme);
+            },
+          ),
+          ListTile(
+            title: const Text('Language'),
+            trailing: const Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              alignment: WrapAlignment.center,
+              children: [
+                Text('English'),
+                Icon(Icons.chevron_right, size: 24.0)
+              ],
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Currency'),
+            trailing: const Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              alignment: WrapAlignment.center,
+              children: [Text('USD'), Icon(Icons.chevron_right, size: 24.0)],
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('About'),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -6,7 +6,46 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sign In'),),
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                const TextField(
+                  decoration: InputDecoration(
+                    label: Text('Email'),
+                    hintText: 'Email',
+                    prefixIcon: Icon(Icons.email)
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    label: Text('Password'),
+                    hintText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.visibility_off_outlined),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Sign In'),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
